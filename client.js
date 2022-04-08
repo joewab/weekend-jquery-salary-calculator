d5050c52a802976247fa99d50fb149e84f83a883
Joe AB <joe.anthonybrown@gmail.com>
2022-04-08 17:09:49 -0500

    the delete buttons remove rows from the table on the DOM

--- client.js
+++ client.js
@@ -9,48 +9,59 @@ function begin(){
     $('#submitButton').on ('click', addEmployee);
 }
 
+
 function addEmployee(){
+    //create an employee object using the input from the DOM as properties
     let employee = {
         firstName: $('#firstNameInput').val(),
         lastName: $('#lastNameInput').val(),
         id: $('#idInput').val(),
         title: $('#titleInput').val(),
         annualSalary: $('#annualSalaryInput').val()
-    };
+    }; //end create employee object
 
-   employees.push(employee);
-
-   monthlyCost += Number(employee.annualSalary);
-
-$('#monthlyTotal').text(monthlyCost);
-
-   console.log(Number(monthlyCost));
-
-   if (monthlyCost > 20000){
-    $('#tableFooter').css("background-color", "red");
-   }
-
-    $('#employeeInformation').append(
-        `<tr>
+    //append the properites of the employee object into the table formatting on the DOM
+    $(`#employeeInformation`).append(
+        `<tr id='${employee.id}'/>
             <td>${employee.firstName}</td>
             <td>${employee.lastName}</td>
             <td>${employee.id}</td>
             <td>${employee.title}</td>
             <td>${employee.annualSalary}</td>
-            <td><button id="deleteButton">delete</button></td>
+            <td><button id="${employee.id}deleteButton">delete</button></td>
         </tr>`
-
-    )
-
+    ); //end append employee object properties
+    
+    //empty the input fields on the dom
     $('#firstNameInput').val('')
     $('#lastNameInput').val('')
     $('#idInput').val('')
     $('#titleInput').val('')
     $('#annualSalaryInput').val('')
+    //end empty input fields
 
-    $('#deleteButton').on ('click', deleteEmployee);
+    //push employee object into an array
+    employees.push(employee);
+    //end push employee object
+
+    //increment a variable that keeps track of the monthly cost based on the annual salaries
+    //and put that variable on the DOM
+    //if the monthly cost gets above 20000, turn that part of the DOM red
+    monthlyCost += (Number(employee.annualSalary)/12);
+    $('#monthlyTotal').text(monthlyCost.toFixed(2));
+    if (monthlyCost > 20000){
+    $('#tableFooter').css("background-color", "red");
+    };
+    //end update the monthly cost
+
+    //initialize the delete button that is created by this function
+    $(`#${employee.id}deleteButton`).on ('click', deleteEmployee);
+    //end button initialization
 }
 
+
+//run this when a delete button is hit to delete an entire row from the table
 function deleteEmployee(){
-    console.log('deleted');
+    $(this).remove();
 }
+//end delete row
